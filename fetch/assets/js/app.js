372af640fd355b90de3bf2e6263f11d1682c9e5a
TedMartins <ted.freela@gmail.com>
2022-03-14 10:49:24 -0300

    Envio Fetch API

---
+++ fetch/assets/js/app.js
@@ -0,0 +1,29 @@
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+const url = 'https://randomuser.me/api/?results=7&?nat=br,us,uk';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last} | ${author.cell}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
